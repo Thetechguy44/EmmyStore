9fb4bc854d684b17a1fe22edfb18e4b38f7bdbf1
DevGana <yahaya128173908@gmail.com>
2025-08-27 00:20:09 +0100

    add more route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BannerController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthenticationSessionController;
 
@@ -28,5 +30,17 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/edit-product/{product}', [ProductController::class, 'edit'])->name('edit.product');
     Route::put('/update-product/{product}', [ProductController::class, 'update'])->name('update.product');
     Route::delete('/delete-product/{product}', [ProductController::class, 'destroy'])->name('delete.product');
-    // Add more authenticated routes here
+
+    //Route for profile
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/change-password', [ProfileController::class, 'changePassword'])->name('profile.change.password');
+
+    // Route for banners
+    Route::get('/banners', [BannerController::class, 'index'])->name('banners');
+    Route::get('/create-banner', [BannerController::class, 'create'])->name('create.banner');
+    Route::post('/store-banner', [BannerController::class, 'store'])->name('store.banner');
+    Route::get('/edit-banner/{banner}', [BannerController::class, 'edit'])->name('edit.banner');
+    Route::put('/update-banner/{banner}', [BannerController::class, 'update'])->name('update.banner');
+    Route::delete('/delete-banner/{banner}', [BannerController::class, 'destroy'])->name('delete.banner');
 });
