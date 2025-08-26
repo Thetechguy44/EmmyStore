a242f4cd101bd84c1c379aebbb4102b83e2bbb97
DevGana <yahaya128173908@gmail.com>
2025-08-27 00:20:28 +0100

    make dashboard dynamic

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Banner;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        // This method will return the dashboard view
-        return view('dashboard.index');
+        $banners = Banner::latest()->get();
+        $products = Product::latest()->get();
+        return view('dashboard.index', compact('banners', 'products'));
     }
 
     // Add more methods for handling dashboard-related logic here
