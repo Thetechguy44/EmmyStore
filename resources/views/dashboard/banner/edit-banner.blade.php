f220c29cfa933fbc856a19b256f1e6d955d119f7
DevGana <yahaya128173908@gmail.com>
2025-08-27 00:18:54 +0100

    create edit banner blade

---
+++ resources/views/dashboard/banner/edit-banner.blade.php
@@ -0,0 +1,74 @@
+@extends('dashboard.layout.app')
+@section('title', 'Edit Banner')
+@section('content')
+    <div class="content">
+        <!-- Start Content-->
+        <div class="container-fluid">
+
+            <div class="py-3 d-flex align-items-sm-center flex-sm-row flex-column">
+                <div class="flex-grow-1">
+                    <h4 class="fs-18 fw-semibold m-0">Edit Banner</h4>
+                </div>
+
+                {{-- <div class="text-end">
+                    <ol class="breadcrumb m-0 py-0">
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Forms</a></li>
+                        <li class="breadcrumb-item active">Add Banner</li>
+                    </ol>
+                </div> --}}
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-12">
+                    <div class="card">
+
+                        <div class="card-header">
+                            <h5 class="card-title mb-0">Edit Banner</h5>
+                        </div>
+                        <!-- end card header -->
+                        
+                        <div class="card-body">
+                            @if (Session::has('success'))
+                                <div class="toast show align-items-center text-white bg-primary border-0 mb-4" role="alert" aria-live="assertive" aria-atomic="true">
+                                    <div class="d-flex">
+                                        <div class="toast-body">
+                                            {{ Session::get('success') }}
+                                        </div>
+                                        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                                    </div>
+                                </div>
+                            @endif
+                            <form action="{{ route('update.banner', $banner->id) }}" method="POST" enctype="multipart/form-data" id="productForm">
+                                @csrf
+                                @method('PUT')
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="title" class="form-label">Title</label>
+                                        <input type="text" id="title" name="title" class="form-control" value="{{ $banner->title }}" required>
+                                    </div>
+                                </div>
+                                
+                                <div class="mb-4 col-md-6">
+                                    <img src="{{ asset('images/banners/' . $banner->image_path) }}" alt="Banner Image" class="img-fluid">
+                                </div>
+                                
+                                <div class="mb-4 col-md-6">
+                                    <label class="form-label">Banner Image</label>
+                                    <div class="input-group">
+                                        <input type="file" class="form-control" id="bannerImage" name="image" accept="image/*">
+                                        <label class="input-group-text" for="bannerImage">Upload</label>
+                                    </div>
+                                </div>
+                                
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- container-fluid -->
+    </div>
+@endsection
